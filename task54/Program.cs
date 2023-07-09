434659f84ec09467827c7dbf00588a0645d7ef2d
Kirill Grinkov <79636456163@yandex.ru>
2023-07-09 23:07:23 +0400

    addtask54

---
+++ task54/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите минимальное значение массива: ");
+int minNum = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите максимальное значение массива: ");
+int maxNum = int.Parse(Console.ReadLine() ?? "");
+
+int[,] array = new int[rows, columns];
+
+GetRandomArray(array);
+
+PrintArray(array);
+
+Console.WriteLine();
+
+streamlineArray(array);
+
+PrintArray(array);
+
+
+void GetRandomArray (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(minNum, maxNum);
+        }
+    }
+}
+
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void streamlineArray(int[,]array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int findMax= array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = findMax;
+                }
+}
+
+
