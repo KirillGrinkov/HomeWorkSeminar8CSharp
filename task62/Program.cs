5155b82598c81d6d9e0402d54b8e4d23ae3b3914
Kirill Grinkov <79636456163@yandex.ru>
2023-07-11 09:57:38 +0400

    addtask62

---
+++ task62/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 62: Заполните спирально массив 4 на 4.
+
+Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+
+
+int[,] array = new int[rows, columns];
+
+
+streamlineArray(array);
+
+PrintArray(array);
+
+
+void streamlineArray(int[,]array)
+{
+    int temp = 1;
+    int i = 0;
+    int j = 0;
+
+    while (temp <= array.GetLength(0) * array.GetLength(1))
+    {
+        array[i, j] = temp;
+        temp++;
+        if (i <= j + 1 && i + j < array.GetLength(1) - 1)
+            j++;
+        else if (i < j && i + j >= array.GetLength(0) - 1)
+            i++;
+        else if (i >= j && i + j > array.GetLength(1) - 1)
+            j--;
+        else
+            i--;
+    }
+}
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
